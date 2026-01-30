fcbc7cd1d332aedc2ccdd3d39843476f9a33d620
simba.wang <simba1986.wang@gmail.com>
2026-01-30 13:59:13 +0800

    [FIX] update detectron2 and chumpy installation instructions in setup.py

--- setup.py
+++ setup.py
@@ -15,8 +15,16 @@ setup(
         'smplx==0.1.28',
         'yacs',
         #'detectron2 @ git+https://github.com/facebookresearch/detectron2',
-        'detectron2 @ git+https://github.com/facebookresearch/detectron2.git@ff53992b1985b63bd3262b5a36167098e3dada02',
-        'chumpy @ git+https://github.com/mattloper/chumpy',
+        '--no-build-isolation detectron2 @ git+https://github.com/facebookresearch/detectron2.git@ff53992b1985b63bd3262b5a36167098e3dada02',
+        # if there is no torch found when installing detectron2 even though torch is already installed,
+        # please install detectron2 following:
+        # git clone https://github.com/facebookresearch/detectron2.git
+        # git checkout ff53992b1985b63bd3262b5a36167098e3dada02
+        # conda install -c conda-forge gcc=11 gxx=11 -n hamer 
+        # export CC=$CONDA_PREFIX/bin/gcc                                                   
+        # export CXX=$CONDA_PREFIX/bin/g++ 
+        # python -m pip install -e detectron2 --no-build-isolation 
+        '--no-build-isolation chumpy @ git+https://github.com/mattloper/chumpy',
         'mmcv==1.3.9',
         'timm',
         'einops',
